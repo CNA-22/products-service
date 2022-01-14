d04f34856c931549173c3fa8b14c5fccdc0c3145
Kraakan <60356996+Kraakan@users.noreply.github.com>
2022-01-14 18:27:32 +0200

    trying databse connection

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const port = process.env.PORT;
+const test_db = process.env.DB_URI_TESTING;
+
+// Mongoose connection 
+const mongoose = require('mongoose')
+mongoose.connect(test_db)
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to DB'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
