c8b818369c2f656cf7d761d6544f33eec63728f0
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2022-01-23 05:57:35 +0200

    Add scripts/createToken.js

---
+++ scripts/createToken.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const path = require('path');
+
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env.development'),
+});
+
+(async () => {
+    const secret = process.env.JWT_SECRET;
+    if (secret == null) {
+        throw new Error('The environment variable JWT_SECRET was not defined.');
+    }
+    console.log(jwt.sign('developmentToken', process.env.JWT_SECRET));
+})().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
