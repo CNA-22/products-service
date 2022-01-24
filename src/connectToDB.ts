277ce9eba5ae5bc303769e14b2a3c0f604bc9449
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2022-01-24 14:57:08 +0200

    Change DB connection configuration

--- src/connectToDB.ts
+++ src/connectToDB.ts
@@ -3,15 +3,17 @@ import mongoose from "mongoose";
 import {allFulfilled, requireEnvVar} from "./utils";
 
 const credentialsToEnvVarNames = [
+    ["protocol", "MONGO_PROTOCOL"],
     ["rootUsername", "MONGO_INITDB_ROOT_USERNAME"],
     ["rootPassword", "MONGO_INITDB_ROOT_PASSWORD"],
     ["containerName", "MONGO_CONTAINER_NAME"],
     ["dbName", "MONGO_INITDB_DATABASE"],
+    ["query", "MONGO_QUERY"],
 ];
 
 // From https://github.com/DanielGiljam/ia-2-017-0-lodge-broker/blob/d63e934e69fb3806abd3b3991578f1b9f45becfc/src/connectToDB.ts#L12
 const connectToDB = async (): Promise<void> => {
-    const {rootUsername, rootPassword, containerName, dbName} =
+    const {protocol, rootUsername, rootPassword, containerName, dbName, query} =
         Object.fromEntries(
             await allFulfilled(
                 credentialsToEnvVarNames.map(
@@ -27,7 +29,7 @@ const connectToDB = async (): Promise<void> => {
             }),
         );
     await mongoose.connect(
-        `mongodb://${rootUsername}:${rootPassword}@${containerName}/${dbName}?authMechanism=SCRAM-SHA-1&authSource=admin`,
+        `${protocol}://${rootUsername}:${rootPassword}@${containerName}/${dbName}?${query}`,
         {ignoreUndefined: true},
     );
     console.log("Successfully connected to MongoDB.");
