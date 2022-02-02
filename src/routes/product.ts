0b390a337b00815b59406134c04488c654d872a7
Alexander Nylund <55355086+slightly-blue@users.noreply.github.com>
2022-02-02 18:44:42 +0200

    Add Image POST, DELETE endpoints
    + Cloudinary integration
    + Windows repo compatibility fixes
    + (husky solution require symlink between Github desktop's git folder and npm's node modules )

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,5 +1,8 @@
-import {RequestHandler, Router} from "express";
+import cloudinary from "cloudinary";
+import {Request, RequestHandler, Router} from "express";
 import {JSONSchema7} from "json-schema";
+import multer from "multer";
+import streamifier from "streamifier";
 
 import requestBodyValidator from "../middlewares/requestBodyValidator";
 import Product from "../models/Product";
@@ -23,6 +26,55 @@ const generateId = async (): Promise<string> => {
         return await generateId();
     }
 };
+
+const imageUploadReceiver: RequestHandler = async (req, res, next) => {
+    upload.single("image")(req, res, (err) => {
+        if (err !== undefined) {
+            res.status(400).json({msg: String(err)});
+        } else {
+            if (req.file! === undefined) {
+                res.status(400).json({msg: "No file provided"});
+            } else {
+                next();
+            }
+        }
+    });
+};
+
+// credit: https://support.cloudinary.com/hc/en-us/community/posts/360007581379-Correct-way-of-uploading-from-buffer-
+const uploadFromBuffer = async (req: Request): Promise<any> => {
+    return await new Promise((resolve, reject) => {
+        const cldUploadStream = cloudinary.v2.uploader.upload_stream(
+            (error: any, result: any) => {
+                if (result !== undefined) {
+                    console.log(result);
+                    resolve(result);
+                } else {
+                    reject(error);
+                }
+            },
+        );
+        streamifier.createReadStream(req.file!.buffer).pipe(cldUploadStream);
+    });
+};
+
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 1 * 1024 * 1024}, // 1Mb
+    fileFilter: (_req, file, cb) => {
+        if (
+            file.mimetype === "image/png" ||
+            file.mimetype === "image/jpg" ||
+            file.mimetype === "image/jpeg"
+        ) {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+        }
+    },
+});
 
 const productRequestBodySchema: JSONSchema7 = {
     type: "object",
@@ -100,6 +152,59 @@ product.delete("/:productId", findProduct, async (req, res) => {
     delete deletedProduct._id;
     delete deletedProduct.packageDimensions._id;
     res.status(200).json(deletedProduct);
+});
+
+product.post(
+    "/:productId/image",
+    findProduct, // check that product exist
+    imageUploadReceiver, // validate and load image data
+    async (req, res) => {
+        try {
+            const cloudinaryResponse = await uploadFromBuffer(req); // send image to cloudinary and await response
+            const product = req.product!;
+            product.imageURLs.push(cloudinaryResponse.secure_url);
+
+            const updatedProduct = (await product.save()).toObject(); // add image URL to DB
+            delete updatedProduct.__v;
+            delete updatedProduct._id;
+            // @ts-expect-error
+            delete updatedProduct.packageDimensions._id;
+            res.status(200).json(updatedProduct);
+        } catch (err) {
+            console.log(err);
+            res.status(500);
+        }
+    },
+);
+
+product.delete("/:productId/image", findProduct, async (req, res) => {
+    const product = req.product!;
+    const image = req.body.image!;
+
+    if (product.imageURLs.includes(image)) {
+        try {
+            // Removing images from cloudinary will burn trough the free quota much faster than just letting them gather
+            // Therefore the feature to remove images from cloudinary exist but is out-commented
+            //
+            // const cloudinaryImageId = String(image.split("/").slice(-1)[0]!.split(".")[0])
+            // const cloudinaryResponse = await cloudinary.v2.api.delete_resources(cloudinaryImageId);
+
+            product.imageURLs = product.imageURLs.filter((item) => {
+                return item !== image;
+            });
+            const updatedProduct = (await product.save()).toObject();
+            delete updatedProduct.__v;
+            delete updatedProduct._id;
+            // @ts-expect-error
+            delete updatedProduct.packageDimensions._id;
+            res.status(200).json(updatedProduct);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send();
+        }
+    } else {
+        res.status(404).send();
+    }
 });
 
 export default product;
